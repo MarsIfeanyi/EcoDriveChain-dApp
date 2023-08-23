d88487faa399bef2fa88ec0bd97d51a0d17245b8
MarsIfeanyi <marschalice@gmail.com>
2023-08-23 22:53:36 +0100

    Update contract

--- smartContract/contracts/EcoDriveChain.sol
+++ smartContract/contracts/EcoDriveChain.sol
@@ -1,142 +1,85 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {rEDCToken} from "./rEDCToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
- * @title EcoDriveChain Smart Contract
+ * @title Factory contract that deploys the rEDCToken and mints the Receipt Tokens to the user upon deposit of Pet Bottles to the Pool
  * @author Marcellus Ifeanyi
- * @notice This is a Smart Contract that rewards users for proper 
+ * @notice Check the PRD.md for the requirements
  */
+
 contract EcoDriveChain {
     struct User {
         string email;
         string password;
-        address uSDCWalletAddress;
+        address payable usdcWalletAddress;
     }
+    User user;
 
+    address poolAdmin;
+    address[] users;
+    mapping(address => User) public addressToUser;
+    mapping(address => uint) public addressToBottleQuantity;
     mapping(address => rEDCToken) public receiptTokens;
-    mapping(address => uint256) public userToAmountDeposited;
-    mapping(address => bool) public userDepositStatus;
-    mapping(string => address) public userEmailToAccount;
-    mapping(address => User) public userAccounts;
-    mapping(address => IERC20) public poolUSDCWallet;
+    mapping(address => IERC20) public tokens;
 
-    address public poolAdmin;
-    address public circleAPI;
+    // uint256 INITIAL_SUPPLY = 1_000_000_000;
 
-    modifier isValidAddress(address _user) {
-        require(_user != address(0), "ERC20: Not a Valid Address");
-        _;
+    uint256 public exchangeRateInUSDC;
+
+    constructor() {
+        exchangeRateInUSDC = 1; //  0.1 USDC = 1 Pet Bottle
     }
 
-    modifier onlyPoolAdmin() {
-        require(
-            msg.sender == poolAdmin,
-            "Only the Pool Admin can call this function"
-        );
-        _;
-    }
-
-    event DepositSuccessful(address indexed user, uint256 amount);
-    event DepositValidation(address indexed user, bool isValidated);
-    event ReceiptRedeemed(address indexed user, uint256 amount);
-    event UserSignup(address indexed user, string email, address wallet);
-
-    constructor(address _poolAdmin, address _circleAPI) {
-        poolAdmin = _poolAdmin;
-        circleAPI = _circleAPI;
-    }
-
-    function signUp(
-        string memory _email,
-        string memory _password,
-        address _wallet
-    ) external {
-        require(
-            userEmailToAccount[_email] == address(0),
-            "User with this email already exists"
-        );
-
-        // Perform validations on email, password, and wallet address
-
-        // Save the user account information
-        userEmailToAccount[_email] = msg.sender;
-        userAccounts[msg.sender] = User(_email, _password, _wallet);
-
-        emit UserSignup(msg.sender, _email, _wallet);
-    }
+    event petBottleDeposited(
+        address user,
+        uint petBottleQty,
+        address uSDCWalletAddr
+    );
 
     function depositPetBottle(
-        uint256 _amount
-    ) external isValidAddress(msg.sender) {
-        require(
-            userDepositStatus[msg.sender] == false,
-            "User already made a deposit"
+        address payable uSDCWalletAddr,
+        uint petBottleQty
+    ) external {
+        // users.push(uSDCWalletAddr);
+        addressToUser[msg.sender].usdcWalletAddress = uSDCWalletAddr;
+        addressToBottleQuantity[msg.sender] += petBottleQty;
+
+        receiptTokens[msg.sender] = new rEDCToken(
+            "Receipt EDC",
+            "rDEC"
+            //INITIAL_SUPPLY
         );
 
-        User memory user = userAccounts[msg.sender];
-        require(
-            user.uSDCWalletAddress != address(0),
-            "User wallet not created"
-        );
+        receiptTokens[msg.sender].mintToken(msg.sender, petBottleQty);
 
-        // Mint the Receipt Token to the Depositor
-        receiptTokens[msg.sender].mintToken(msg.sender, _amount);
-
-        // Update the amount deposited by the user
-        userToAmountDeposited[msg.sender] += _amount;
-        userDepositStatus[msg.sender] = true;
-
-        emit DepositSuccessful(msg.sender, _amount);
+        emit petBottleDeposited(msg.sender, petBottleQty, uSDCWalletAddr);
     }
 
-    // function createUSDCWallet() external isValidAddress(msg.sender) {
-    //     User storage user = userAccounts[msg.sender];
-    //     require(user.wallet == address(0), "User wallet already created");
+    function redeemReceiptTokens(uint tokensToRedeem) external payable {
+        // require(
+        //     tokensToRedeem > 0,
+        //     " Pet Bottle Qty To Redeem must be greater than zero"
+        // );
+        // require(
+        //     addressToBottleQuantity[msg.sender] >= tokensToRedeem,
+        //     "Insufficient tokensToRedeem"
+        // );
+        uint uSDCAmount = tokensToRedeem * exchangeRateInUSDC;
 
-    //     // Call the Circle API to create a USDC wallet for the user
-    //     // ...
-
-    //     user.wallet = createdWalletAddress; // Assign the created wallet address
-
-    //     emit UserWalletCreated(msg.sender, user.wallet);
-    // }
-
-    function validateDeposit(
-        address _user,
-        bool _isValidated
-    ) external onlyPoolAdmin {
-        require(
-            userDepositStatus[_user] == true,
-            "No deposit made by the user"
+        receiptTokens[msg.sender] = new rEDCToken(
+            "Receipt EDC",
+            "rDEC"
+            //INITIAL_SUPPLY
         );
 
-        emit DepositValidation(_user, _isValidated);
-    }
+        receiptTokens[msg.sender].burnToken(msg.sender, tokensToRedeem);
+        addressToBottleQuantity[msg.sender] -= tokensToRedeem;
+        payable(msg.sender).transfer(uSDCAmount);
 
-    function redeemReceiptToken(
-        uint256 _amount
-    ) external isValidAddress(msg.sender) {
-        require(
-            userDepositStatus[msg.sender] == true,
-            "No deposit made by the user"
-        );
-        require(
-            receiptTokens[msg.sender].balanceOf(msg.sender) >= _amount,
-            "Insufficient receipt token balance"
-        );
-
-        // Burn the Receipt Token provided by the Depositor on Withdrawal
-        receiptTokens[msg.sender].burnToken(msg.sender, _amount);
-
-        // TODO: Call the Circle API to transfer the USDC equivalent amount to the user's USDC wallet
-        bool success = poolUSDCWallet[
-            userAccounts[msg.sender].uSDCWalletAddress
-        ].transfer(msg.sender, _amount);
-        require(success, "Transfer failed");
-
-        emit ReceiptRedeemed(msg.sender, _amount);
+        // TODO: Transfer USDC equivalent of the burnt rToken  to the user. Chainlink oracle will be used here to determine the conversion rate.
     }
 }

8c66d421bfc2eb4b35aba96b3f8cb3bd8dea7e8b
MarsIfeanyi <marschalice@gmail.com>
2023-08-23 23:54:52 +0100

    Add constant folder for contractAddress and contractABI

---
+++ frontEnd/constant/constants.js
@@ -0,0 +1,164 @@
+export const contractAddress = "0x4221D9Ba0Ba0d3070c308bc8E62b66846fd3a379 ";
+
+export const contractABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "petBottleQty",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "uSDCWalletAddr",
+        type: "address",
+      },
+    ],
+    name: "petBottleDeposited",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "addressToBottleQuantity",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "addressToUser",
+    outputs: [
+      {
+        internalType: "string",
+        name: "email",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+      {
+        internalType: "address payable",
+        name: "usdcWalletAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address payable",
+        name: "uSDCWalletAddr",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "petBottleQty",
+        type: "uint256",
+      },
+    ],
+    name: "depositPetBottle",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "exchangeRateInUSDC",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "receiptTokens",
+    outputs: [
+      {
+        internalType: "contract rEDCToken",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokensToRedeem",
+        type: "uint256",
+      },
+    ],
+    name: "redeemReceiptTokens",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "tokens",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];

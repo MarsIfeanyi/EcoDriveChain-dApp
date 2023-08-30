64690a74c8b81f493869e5474c9b0ee3f0671e8f
MarsIfeanyi <marschalice@gmail.com>
2023-08-30 07:31:34 +0100

    Add RainbowKitWagmiCongif

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -20,47 +20,93 @@ import SendFundsConfirm from "./Page/Wallet/WalletComponents/SendFundsConfirm/Se
 import ConvertFunds from "./Page/Wallet/WalletComponents/ConvertFunds/ConvertFunds";
 
 import LeaderMain from "./Layout/LeaderMain/LeaderMain";
-import DepositPetBottlePage from "./Page/ContractInteractions/DepositPetBottlePage";
+import RecyclePetBottlesPage from "./Page/ContractInteractions/RecyclePetBottlesPage";
+import RainbowKitWagmiConfigs from "./RainbowKitWagmiConfigs";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+  midnightTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+  polygonMumbai,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygonMumbai, polygon, optimism, arbitrum, base, zora],
+  [
+    alchemyProvider({
+      apiKey: import.meta.env.VITE_REACT_APP_MUMBAI_API_KEY,
+    }),
+    jsonRpcProvider({ rpc: () => ({ http: "https://rpc.ankr.com/gnosis" }) }),
+    publicProvider(),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: "EcoDriveChain dApp",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="dashboard" element={<PrivateRoutes />}>
-          <Route path="" element={<DashMain />} />
-          <Route path="leaderboard" element={<LeaderMain />} />
-          <Route path="start-recycling" element={<RecycleMain />} />
-          <Route
-            path="start-recycling/deposit-bottles"
-            element={<DepositPetBottlePage />}
-          />
+      <RainbowKitWagmiConfigs>
+        <Routes>
+          <Route path="dashboard" element={<PrivateRoutes />}>
+            <Route path="" element={<DashMain />} />
+            <Route path="leaderboard" element={<LeaderMain />} />
+            <Route path="start-recycling" element={<RecycleMain />} />
+            <Route
+              path="start-recycling/recycle-onchain"
+              element={<RecyclePetBottlesPage />}
+            />
 
-          <Route path="wallet" element={<Wallet />}>
-            <Route path="" element={<WalletMain />} />
-            <Route path="convert-funds" element={<ConvertFunds />} />
-            <Route path="send-funds" element={<SendFunds />} />
-            <Route path="send-funds-amount" element={<SendFundsAmount />} />
-            <Route path="send-funds-confirm" element={<SendFundsConfirm />} />
+            <Route path="wallet" element={<Wallet />}>
+              <Route path="" element={<WalletMain />} />
+              <Route path="convert-funds" element={<ConvertFunds />} />
+              <Route path="send-funds" element={<SendFunds />} />
+              <Route path="send-funds-amount" element={<SendFundsAmount />} />
+              <Route path="send-funds-confirm" element={<SendFundsConfirm />} />
+            </Route>
+
+            <Route path="settings" element={<Settings />}>
+              <Route path="" element={<EditPersonal />} />
+              <Route path="password" element={<EditPassword />} />
+            </Route>
           </Route>
+          {/* <Route path="" element={<PrivateRoutes />}> */}
+          {/* <Route path="/start-recycling" element={<Recycle />} /> */}
+          {/* <Route path="/start-recycling" element={<RecycleMain />} /> */}
+          {/* </Route> */}
 
-          <Route path="settings" element={<Settings />}>
-            <Route path="" element={<EditPersonal />} />
-            <Route path="password" element={<EditPassword />} />
+          <Route path="/" element={<PublicRoute />}>
+            <Route path="" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
           </Route>
-        </Route>
-        {/* <Route path="" element={<PrivateRoutes />}> */}
-        {/* <Route path="/start-recycling" element={<Recycle />} /> */}
-        {/* <Route path="/start-recycling" element={<RecycleMain />} /> */}
-        {/* </Route> */}
+        </Routes>
 
-        <Route path="/" element={<PublicRoute />}>
-          <Route path="" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-
-      <Footer />
+        <Footer />
+      </RainbowKitWagmiConfigs>
     </>
   );
 }
